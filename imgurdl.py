87ef4ee30a8c85435bda4d94c37f57762714a952
Michał Dziadowicz <michal@dziadowicz.it>
2019-02-10 21:34:13 +0100

    Add downloading images from imgur gallery url

--- imgurdl.py
+++ imgurdl.py
@@ -1,9 +1,22 @@
 import sys
+import re
+import requests
+import json
 
 
 def main(args):
-    pass
+    if len(args):
+        result = requests.get(args[0])
+        if result.status_code == 200:
+            match = re.search(r"\n\s*image\s*:\s*({.*})", result.text)
+            if match:
+                for image in json.loads(match.group(1))['album_images']['images']:
+                    result = requests.get('https://i.imgur.com/' + image['hash'] + image['ext'])
+                    if result.status_code == 200:
+                        filename = image['hash'] + image['ext']
+                        with open(filename, 'wb') as file:
+                            file.write(result.content)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(sys.argv[1:])
