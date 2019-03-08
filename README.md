c5716bc6147bdca9901037eb3736260badb42dea
Michał Dziadowicz <michal@dziadowicz.it>
2019-03-08 22:22:29 +0100

    Add downloading to a specified directory

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ A simple Python script for downloading the images from imgur.com.
 To download all images from imgur gallery pass gallery link as first argument:
 
     imgurdl.py https://imgur.com/gallery/xxxxxxx
+
+To download all images to a specified directory (absolute or relative) pass directory path as second argument:
+
+    imgurdl.py https://imgur.com/gallery/xxxxxxx path_to_save
--- imgurdl.py
+++ imgurdl.py
@@ -2,6 +2,7 @@ import sys
 import re
 import requests
 import json
+import os
 
 
 def main(args):
@@ -10,11 +11,17 @@ def main(args):
         if result.status_code == 200:
             match = re.search(r"\n\s*image\s*:\s*({.*})", result.text)
             if match:
+                directory = ''
+                try:
+                    os.makedirs(args[1])
+                    directory = args[1]
+                except IndexError:
+                    pass
                 for image in json.loads(match.group(1))['album_images']['images']:
                     result = requests.get('https://i.imgur.com/' + image['hash'] + image['ext'])
                     if result.status_code == 200:
                         filename = image['hash'] + image['ext']
-                        with open(filename, 'wb') as file:
+                        with open(os.path.join(directory, filename), 'wb') as file:
                             file.write(result.content)
 
 
